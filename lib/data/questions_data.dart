5fa3530f9d0c6ef5612250f08a4b7665d204d4c3
Yosoph Ahmed <devyosoph@gmail.com>
2023-01-18 13:00:03 +0200

    questions data list

---
+++ lib/data/questions_data.dart
@@ -0,0 +1,157 @@
+import '../models/question_model.dart';
+
+class QuestionsData {
+  static final List<QuestionModel> questions = [
+    QuestionModel(
+      question: '''1 – What was the score in the Euro 2012 final?''',
+      answer: {
+        '2-0': false,
+        '3-0': false,
+        '4-0': true,
+        '5-0': false,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''2 – Football Player Quiz: Who won the Man of the Match award in the 2014 World Cup final?''',
+      answer: {
+        'Mario Goetze': true,
+        'Sergio Aguero': false,
+        'Lionel Messi': false,
+        'Bastian Schweinsteiger': false,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''3 – Against which country did Wayne Rooney break the England goalscoring record?''',
+      answer: {
+        'Switzerland': true,
+        'San Marino': false,
+        'Lithuania': false,
+        'Slovenia': false,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''4 – After losing a key player in the first game, which team went onto the semi-finals of Euro 2020?''',
+      answer: {
+        'Denmark': true,
+        'Spain': false,
+        'Wales': false,
+        'England': false,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''5 – Which footballer holds the record for the highest number of assists in the Premier League?''',
+      answer: {
+        'Cesc Fabregas': false,
+        'Ryan Giggs': true,
+        'Frank Lampard': false,
+        'Paul Scholes': false,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''6 – Which former Belarus international played for Arsenal between 2005 and 2008?''',
+      answer: {
+        'Alexander Hleb': true,
+        'Maksim Romaschenko': false,
+        'Valyantsin Byalkevich': false,
+        'Yuri Zhenov': false,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''7 – Jamie Vardy was signed by Leicester from which non-league side?''',
+      answer: {
+        'Ketting Town': false,
+        'Alfreton Town': false,
+        'Grimsby Town': false,
+        'Fleetwood Town': true,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''8 – Chelsea beat which team 8-0 to secure the 2009-10 Premier League title on the final day of the season?''',
+      answer: {
+        'Blackburn': false,
+        'Hull': false,
+        'Wigan': true,
+        'Norwich': false,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''9 – Who is the current top scorer in the UEFA Champions League?''',
+      answer: {
+        'Alan Shearer': false,
+        'Thierry Henry': false,
+        'Cristiano Ronaldo': true,
+        'Robert Lewandowski': false,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''10 – Manchester United beat which team in the 2017 Europa League final?''',
+      answer: {
+        'Villarreal': false,
+        'Chelsea': false,
+        'Ajax': true,
+        'Borussia Dortmund': false,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''11 – Gareth Bale’s breakthrough moment came in the 2010-11 season, when he scored a second half hat-trick against which team?''',
+      answer: {
+        'Inter Milan': false,
+        'AC Milan': true,
+        'Juventus': false,
+        'Napoli': false,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''12 – Which team did Porto beat in the 2004 Champions League final?''',
+      answer: {
+        'Bayern Munich': false,
+        'Deportivo La Coruña': false,
+        'Barcelona': false,
+        'Monaco': true,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''13 – Which Serbian team scored beat Marseille on penalties to secure the 1991 European Cup?''',
+      answer: {
+        'Slavia Prague': false,
+        'Red Star Belgrade': true,
+        'Galatasaray': false,
+        'Spartak Trnava': false,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''14 – You can find the oldest footballer in the world playing in which country?''',
+      answer: {
+        'Malaysia': false,
+        'Ecudaor': false,
+        'Japan': true,
+        'South Africa': false,
+      },
+    ),
+    QuestionModel(
+      question:
+          '''15 – Which team has won the African Cup of Nations a record 7 times?''',
+      answer: {
+        'Cameroon': false,
+        'Egypt': true,
+        'Senegal': false,
+        'Ghana': false,
+      },
+    ),
+  ];
+
+
+}
