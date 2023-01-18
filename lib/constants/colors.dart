0fdccc9e70bacc56159aea712e7f379a93a82b95
Yosoph Ahmed <devyosoph@gmail.com>
2023-01-18 13:15:03 +0200

    App Colors Map

---
+++ lib/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Map<String,Color> color = {
+    'firstColor' : Color(0xff424874),
+    'secondColor' : Color(0xffA6B1E1),
+    'thirdColor' : Color(0xffDCD6F7),
+    'fourthColor' : Color(0xffF4EEFF),
+    'questionColor' : Color(0xffECECEC),
+    'answersColor' : Color(0xffE3F6FF),
+    'trueAnswerColor' : Color(0xff59CE8F),
+    'falseAnswerColor' : Color(0xffFF1E00),
+  };
+
+}
